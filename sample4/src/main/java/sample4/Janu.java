4dac71854f6dce311c2959e7ffc743a49599b777
jananl <jananl@DIN78000401.corp.capgemini.com>
2019-07-04 14:49:14 +0530

    CHANGED

--- sample4/src/main/java/sample4/Janu.java
+++ sample4/src/main/java/sample4/Janu.java
@@ -4,5 +4,8 @@ public class Janu {
 
 	public static void main(String args)
 {
-	System.out.println("HELLO world");	}
+	System.out.println("HELLO world");
+
+	System.out.println("THIS IS JANANI'S 1ST GIT");
+}
 }
